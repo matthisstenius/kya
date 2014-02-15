9d6d593f955af5f783ebe59a6ba9d2582b1043e2
Matthis Stenius <matthis@me.com>
2014-02-15 15:54:29 +0100

    added author and cat

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-
 <?php get_header(); ?>
-
-
 		<?php while (have_posts() ) : the_post(); ?>
 			
 			<section class="blog-posts clearfix">
 				<a href="<?php the_permalink(); ?>">
 				<div class="col-1-3">
-					
 						<?php
 							if (has_post_thumbnail() ) {
 						 		the_post_thumbnail(); 
@@ -23,7 +19,9 @@
 				
 				<article class="col-2-3">
 					<h2><?php the_title(); ?></h2>
-					
+					<p><?php the_author(); ?></p>
+					<p><?php the_category(); ?></p>
+
 					<?php the_excerpt(); ?>
 					
 				</article>
